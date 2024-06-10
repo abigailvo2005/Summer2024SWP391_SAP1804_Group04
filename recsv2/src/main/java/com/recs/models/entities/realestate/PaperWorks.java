5571eda0d2f814a5bdf04305a1134e853ef3b124
hungvhoang <hunghvse183800@fpt.edu.vn>
2024-06-10 22:03:42 +0700

    feat: handle save - get paperwork and images

--- recsv2/src/main/java/com/recs/models/entities/realestate/PaperWorks.java
+++ recsv2/src/main/java/com/recs/models/entities/realestate/PaperWorks.java
@@ -1,10 +1,7 @@
 package com.recs.models.entities.realestate;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
