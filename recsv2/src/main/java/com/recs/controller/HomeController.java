a3e7941bb4ba156245590b9f8bcffe8ee6cb9971
hungvhoang <hunghvse183800@fpt.edu.vn>
2024-07-09 00:36:47 +0700

    feat: controller reset password

--- recsv2/src/main/java/com/recs/controller/HomeController.java
+++ recsv2/src/main/java/com/recs/controller/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Set;
 
@@ -33,5 +35,25 @@ public class HomeController {
         return "sign-in";
     }
 
+    @GetMapping("/login/forget")
+    public String forgetPasswordView() {
+        return "home";
+    }
 
+    @GetMapping("/login/reset")
+    public String resetPasswordView(
+            @RequestParam String token,
+            Model model
+    ) {
+        model.addAttribute("token", token);
+        return "home";
+    }
+
+    @PostMapping("/login/reset")
+    public String resetPassword(
+            @RequestParam String token,
+            @RequestParam String newPassword
+    ) {
+        return "redirect:/login";
+    }
 }
